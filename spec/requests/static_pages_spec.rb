e0dbef7f2bf5257419cc977ed5dca236b99a8ff1
anthonydanger <anthony.sanchez.sf@gmail.com>
2013-05-08 00:08:23 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,19 +5,17 @@ describe "Static pages" do
   let(:base_title) { "Anon App" }
 
   describe "Home page" do
+    before { visit root_path }
 
   	it "should have the h1 'Anon App'" do
-  		visit '/static_pages/home'
   		page.should have_selector('h1', :text => 'Anon App')
   	end
 
   	it "should have the base title" do
-  		visit '/static_pages/home'
   		page.should have_selector('title', :text => "Anon App")
   	end  
 
     it "should not have a custom page title" do 
-      visit '/static_pages/home'
       page.should_not have_selector('title', :text => '| Home')
     end
   end
@@ -25,12 +23,12 @@ describe "Static pages" do
   describe "Help page" do
 
   	it "should have the h1 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('h1', :text => 'Help')
   	end
 
   	it "should have the Title 'Help'" do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_selector('title', :text => "#{base_title} | Help")
   	end  
   end
@@ -38,24 +36,24 @@ describe "Static pages" do
     describe "About page" do
 
   	it "should have the h1 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('h1', :text => 'About Us')
   	end
 
   	it "should have the Title 'About Us'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_selector('title', :text => "#{base_title} | About Us")
   	end
 
     describe "Contact Us" do
 
     it "should have the h1 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', :text => 'Contact Us')
     end
 
     it "should have the Title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end  
   end
